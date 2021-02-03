2291e2b0f843afec63746917fcb3206a31aacbae
briggitteMartinez <briggitte90@hotmail.com>
2021-02-03 19:23:01 +0100

    Initial Commit

---
+++ MiseEnPlace/MiseEnPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiseEnPlaceApp.swift
+//  MiseEnPlace
+//
+//  Created by Briggitte Quintero Martinez on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct MiseEnPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
