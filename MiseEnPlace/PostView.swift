6f6d9b85bb617903a7be006c9e9a644b43895930
briggitteMartinez <briggitte90@hotmail.com>
2021-02-08 13:13:57 +0100

    recipe postview design

--- MiseEnPlace/PostView.swift
+++ MiseEnPlace/PostView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct PostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Recipe Name Here").font(.title2)
+                Spacer()
+                Image(systemName: "ellipsis").font(.headline)
+            }
+            Image("rotfrukter").resizable().scaledToFit()
+        }
     }
 }
 
