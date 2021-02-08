bd9c72ee8c20db7cc54ec7ebe8f0142574928b26
briggitteMartinez <briggitte90@hotmail.com>
2021-02-08 13:04:32 +0100

    added PostView file

---
+++ MiseEnPlace/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  MiseEnPlace
+//
+//  Created by Briggitte Quintero Martinez on 2021-02-08.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
