7ebf83de7082e2359dc19b0769ab3cdc3ec5e798
briggitteMartinez <briggitte90@hotmail.com>
2021-02-08 12:52:26 +0100

    added ScrollView to RecipesView

--- MiseEnPlace/RecipesView.swift
+++ MiseEnPlace/RecipesView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct RecipesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false, content: {
+                    Text("Placeceholder")
+                    Text("Placeceholder")
+                    Text("Placeceholder")
+                    Text("Placeceholder")
+                }).navigationBarTitle("Recipes")
+                .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct RecipesView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipesView()
+        NavigationView{
+                    RecipesView()
+                }
+
     }
 }
