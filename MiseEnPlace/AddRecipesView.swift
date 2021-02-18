d0f62537b0deccad00aa25c8191f264cbd70f418
briggitteMartinez <briggitte90@hotmail.com>
2021-02-18 21:20:24 +0100

    addrecipeview design updated

--- MiseEnPlace/AddRecipesView.swift
+++ MiseEnPlace/AddRecipesView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Briggitte Quintero Martinez on 2021-02-16.
 //
-
+import UIKit
 import SwiftUI
 
 struct AddRecipesView: View {
@@ -14,17 +14,32 @@ struct AddRecipesView: View {
         ScrollView {
             VStack{
                 Spacer()
-                Spacer()
-                Spacer()
                 Divider()
                 TextField("Title of recipe here", text: $title).padding(.all, 6)
                 Divider()
-                Image(systemName: "photo").resizable().scaledToFit()
+                Image(systemName: "xmark.rectangle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height:300, alignment: .center)
                 Divider()
-                TextEditor(text: $inputText)
-                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {print("take picture")}) {
+                        Image(systemName: "camera")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height:50, alignment: .center)
+                    }
+                    Button(action: { print("uppload photo")}) {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50, alignment: .leading)
+                    }
+                }.padding(6)
+                TextEditor(text: $inputText).frame(width: 370, height: 900, alignment: .center).border(Color.gray, width: 2)
                 
-            }
+            }.accentColor(.gray)
         }.navigationBarTitle("Add a recipe")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing:
@@ -34,7 +49,7 @@ struct AddRecipesView: View {
                                     Text("Save")
                                 }
         )
-
+        
     }
 }
 
